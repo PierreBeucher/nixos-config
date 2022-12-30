790118e347558ed324cff502c3818a84baf0bf49
Pierre Beucher <pierre@crafteo.io>
2022-12-30 15:10:01 +0100

    added Steam

--- modules/os-base.nix
+++ modules/os-base.nix
@@ -83,6 +83,12 @@
     htop
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
   # Docker
   virtualisation.docker = {
     enable = true;
