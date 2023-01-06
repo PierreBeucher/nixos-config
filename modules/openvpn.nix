9bda59f739fc4d78238bce669d0387827b0e1617
Pierre Beucher <pierre@crafteo.io>
2023-01-06 14:10:49 +0100

    force CloudFare DNS 1.1.1.1 / 1.0.0.1

--- modules/openvpn.nix
+++ modules/openvpn.nix
@@ -2,7 +2,7 @@
 
 {
 
-  # OpenVPN
+  # OpenVPN config for ExpressVN
   services.openvpn.servers.expressvpn = {
     config = '' 
         config /home/pbeucher/.openvpn/config-expressvpn.ovpn
@@ -10,5 +10,11 @@
     '';
     autoStart = true;
   };
+
+  # Fix DNS servers to CloudFare
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  
+  # Prevent resolv.conf to be updated by active connection config
+  networking.networkmanager.dns = "none";
 
 }
