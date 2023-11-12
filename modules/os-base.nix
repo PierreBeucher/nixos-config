9b134c9eb7d444775b31ed573d9035c2af2cc430
Pierre Beucher <pierre@crafteo.io>
2023-11-12 10:01:22 +0100

    block config for X

--- modules/os-base.nix
+++ modules/os-base.nix
@@ -34,14 +34,14 @@
   boot.tmp.useTmpfs = true;
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
   services.xserver = {
+    enable = true;
+
+    # Enable the GNOME Desktop Environment.
+    # displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+
+    # Configure keymap in X11
     layout = "fr";
     xkbVariant = "azerty";
   };
