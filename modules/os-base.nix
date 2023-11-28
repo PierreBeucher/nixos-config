c24e996ed5ef7a6826874158d87d4abee2ef6422
Pierre Beucher <pierre@crafteo.io>
2023-11-28 09:14:22 +0100

    reenable gdm

--- modules/os-base.nix
+++ modules/os-base.nix
@@ -38,7 +38,7 @@
     enable = true;
 
     # Enable the GNOME Desktop Environment.
-    # displayManager.gdm.enable = true;
+    displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
 
     # Configure keymap in X11
